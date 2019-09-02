6336011e574ea8ecc3ca3872f9eb876d16a36ad5
Mateusz Adamusiński <maadamusinski@jswits.pl>
2019-09-02 15:01:49 +0200

    version 0.0.1

---
+++ src/java/pl/madamusinski/controller/HomeController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.madamusinski.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author maadamusinski
+ */
+@Controller
+public class HomeController {
+    
+    
+    @RequestMapping(value ={"/"}, method=RequestMethod.GET)
+    public String home(){
+        return "redirect:/index";
+    } 
+    @RequestMapping(value ={"/index"}, method=RequestMethod.GET)
+    public String index(Model model){
+        model.addAttribute("message", "Hello World!");
+        return "index";
+    } 
+}
